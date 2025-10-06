0225cd8ac4326d0aebf45a1d24c49ae089c22c39
shankutanna <147415639+shankutanna@users.noreply.github.com>
2025-10-06 17:56:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
 // set up ======================================================================
 var express = require('express');
-var app = express(); 						// create our app w/ express
-var mongoose = require('mongoose'); 				// mongoose for mongodb
-var port = process.env.PORT || 8080; 				// set the port
-var database = require('./config/database'); 			// load the database config
+var app = express(); 						
+var mongoose = require('mongoose'); 				
+var port = process.env.PORT || 8080; 				
+var database = require('./config/database'); 		
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
 // configuration ===============================================================
-mongoose.connect(database.localUrl); 	// Connect to local MongoDB instance. A remoteUrl is also available (modulus.io)
 
-app.use(express.static('./public')); 		// set the static files location /public/img will be /img for users
-app.use(morgan('dev')); // log every request to the console
-app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
-app.use(bodyParser.json()); // parse application/json
-app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
-app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
+// Use environment variable MONGO_URL if defined, else fallback to database.localUrl
+var mongoUrl = process.env.MONGO_URL || database.localUrl;
 
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Connected to MongoDB at " + mongoUrl))
+    .catch(err => console.error("MongoDB connection error:", err));
+
+app.use(express.static('./public')); 		
+app.use(morgan('dev')); 
+app.use(bodyParser.urlencoded({'extended': 'true'})); 
+app.use(bodyParser.json()); 
+app.use(bodyParser.json({type: 'application/vnd.api+json'})); 
+app.use(methodOverride('X-HTTP-Method-Override')); 
 
 // routes ======================================================================
 require('./app/routes.js')(app);
